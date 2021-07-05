51ff77eee0a9379d8c39fc952f7b335401fc46f6
Nino <gngenius02@gmail.com>
2021-07-04 20:51:12 -0700

    reworked the output to console

--- index.js
+++ index.js
@@ -4,25 +4,13 @@ const { StreamClient } = require('cw-sdk-node')
 
 const client = new StreamClient({
 	creds: { apiKey: '', secretKey: '' },
-	subscriptions: ['markets:579:book:deltas'],
-	logLevel: 'debug'
+	subscriptions: ['markets:579:book:deltas', 'markets:579:book:snapshots'],
+	logLevel: 'none'
 })
 
-const myData = { bids: new Map(), asks: new Map(), seqNum: 0 }
+const getTime = (_) => new Date().toLocaleTimeString()
 
-const processRemoval = (type, price) => {
-	entry = getTwoDigitPrice(price)
-	if (Number(myData[type].get(entry)) > findDifferenceOf) {
-		console.log(
-			type,
-			'DELETED::::\t',
-			Number(myData[type].get(entry)),
-			'\tat Price of:::',
-			Number(entry)
-		)
-	}
-	myData[type].delete(entry)
-}
+const myData = { bids: new Map(), asks: new Map(), seqNum: 0 }
 
 const getDifference = (current, newvalue) => Math.abs(Number(current) - Number(newvalue))
 
@@ -30,61 +18,94 @@ const checkRetraction = (current, newvalue) => Number(newvalue) < Number(current
 
 const getTwoDigitPrice = (price) => Number(price).toFixed(2)
 
+const logdata = {
+	bids: [],
+	asks: []
+}
+
+const logprep = () => {
+	console.clear()
+	const objs = Object.entries(logdata);
+	let [key, data] = objs;
+
+	data = data.splice(0,10)
+	const transformed = data.reduce((acc, { time, ...x }) => {
+			acc[time] = x
+			return acc
+		}, {})
+	console.log(key.toUpperCase())
+	console.table(transformed)
+	}
+}
+
+function shouldReport(curValue, newValue, type, price) {
+	if ((diff = getDifference(curValue, newValue)) > findDifferenceOf) {
+		const op = checkRetraction(curValue, newValue) ? '\u25BC' : '\u25B2'
+		const volume = `${op}${diff.toFixed(8).padStart(14)}`
+		price = `${Number(price).toFixed(2)}`
+		logdata[type].unshift({ time: getTime(), volume, price })
+		logprep()
+	}
+	console.gr
+}
+
+const processRemoval = (type, price) => {
+	entry = getTwoDigitPrice(price)
+	if ((diff = Number(myData[type].get(entry))) > findDifferenceOf) {
+		const op = 'X'
+		const volume = `${op}${diff.toFixed(8).padStart(14)}`
+		price = `${Number(price).toFixed(2)}`
+		logdata[type].unshift({ time: getTime(), volume, price })
+		logprep()
+	}
+	myData[type].delete(entry)
+}
+
 const processEntryBasedOnVolumeAmount = (type, entry) => {
 	let { price, amount: newValue } = entry
 	price = getTwoDigitPrice(price)
 	const curValue = myData[type].get(price) || 0
-	if (getDifference(curValue, newValue) > findDifferenceOf) {
-		if (checkRetraction(curValue, newValue)) {
-			console.log(
-				type,
-				'RETRACTED:::\t',
-				getDifference(curValue, newValue),
-				'\tat Price of :::',
-				Number(price)
-			)
-		} else {
-			console.log(
-				type,
-				'NEW ORDER ::::\t',
-				getDifference(curValue, newValue),
-				'\tat Price of :::',
-				Number(price)
-			)
-		}
-	}
+	shouldReport(curValue, newValue, type, price)
 	return myData[type].set(price, newValue)
 }
 
-const handleNewInstructions = (type, ops) => {
-	const { remove, set } = ops
-	for (let entry of remove) {
-		processRemoval(type, entry)
+const handleNewDelta = (orderBookDelta) => {
+	const { seqNum } = orderBookDelta
+	if (seqNum === myData['seqNum']) {
+		console.log('this seqNum was the same as last one so droping this packet.')
+		return
 	}
-	for (let entry of set) {
-		processEntryBasedOnVolumeAmount(type, entry)
-	}
+	;['bids', 'asks'].forEach((key) => {
+		const { remove, set } = orderBookDelta[key]
+		remove.forEach((entry) => processRemoval(key, entry))
+		set.forEach((entry) => processEntryBasedOnVolumeAmount(key, entry))
+	})
 }
 
-const handleNewDelta = (orderBookDelta) => {
-	if (!orderBookDelta) return
-	const keys = Object.keys(orderBookDelta)
-	for (let key of keys) {
-		if (key === 'seqNum') {
-			if (orderBookDelta[key] === myData[key]) {
-				console.log('this seqNum was the same as last one so droping this packet.')
-				break
-			}
-			myData[key] = orderBookDelta[key]
-			continue
-		}
-		handleNewInstructions(key, orderBookDelta[key])
+const handleNewSnapshot = (orderBookSnapshot) => {
+	const { seqNum } = orderBookSnapshot
+	if (seqNum === myData['seqNum']) {
+		console.log('this seqNum was the same as last one so droping this packet.')
+		return
 	}
+	;['bids', 'asks'].forEach((key) => {
+		for (let { price, amount } of orderBookSnapshot[key]) {
+			if (myData[key].has(price)) {
+				let curValue = myData[key].get(price)
+				if (getDifference(curValue, amount) > findDifferenceOf) {
+					shouldReport(curValue, amount, 'key', price)
+				}
+			}
+			myData[key].set(price, amount)
+		}
+	})
+	console.log('Snapshot Processed.')
 }
 
 // Handlers for market and pair data
 client.onMarketUpdate((marketData) => {
-	handleNewDelta(marketData.orderBookDelta)
+	;(snapshot = marketData?.orderBookSnapshot) ? handleNewSnapshot(snapshot) : null
+	;(delta = marketData?.orderBookDelta) ? handleNewDelta(delta) : null
 })
 
 // Error handling
@@ -96,7 +117,7 @@ const fetch = require('node-fetch')
 const rl = require('readline').createInterface({ input: process.stdin, output: process.stdout })
 
 rl.question('ENTER THRESHOLD VALUE: (Default = 5) ', (threshold) => {
-	if (!threshold) return (findDifferenceOf = 5)
+	if (!threshold || threshold === '') threshold = 5
 	findDifferenceOf = threshold
 
 	fetch('https://api.cryptowat.ch/markets/binance/BTCUSDT/orderbook?limit=5000')
@@ -113,7 +134,7 @@ rl.question('ENTER THRESHOLD VALUE: (Default = 5) ', (threshold) => {
 			}
 			myData['seqNum'] = seqNum
 			console.log('\nBIDS SIZE:', myData.bids.size, '\nASKS SIZE:', myData.asks.size)
-			console.log('starting websocket', myData)
+			// console.log('starting websocket', myData)
 			client.connect()
 		})
 })
